b8973d401f28eeecd92e7bb05de2032a42de62b0
ruslanlap <lapin@ucu.edu.ua>
2024-07-19 21:40:03 +0300

    add easy install process

--- install-not-bin.sh
+++ install-not-bin.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
 # Define variables
-BINARY_NAME="encrypt.sh"
-BINARY_URL="https://raw.githubusercontent.com/ruslanlap/encrypt-decrypt-git/master/encrypt.sh"
-CHECKSUM_URL="https://raw.githubusercontent.com/ruslanlap/encrypt-decrypt-git/master/encrypt.sh.sha256"
-DESTINATION_DIR="$HOME/bin/cryptonit"
+BINARY_NAME="cryptonit"
+BINARY_URL="https://github.com/ruslanlap/encrypt-decrypt-git/releases/download/v1.0.1/cryptonit"
+CHECKSUM_URL="https://raw.githubusercontent.com/ruslanlap/encrypt-decrypt-git/master/cryptonit.sha256"
+DESTINATION_DIR="$HOME/bin"
 DESTINATION_PATH="$DESTINATION_DIR/$BINARY_NAME"
 
 # Create destination directory if it doesn't exist
 mkdir -p "$DESTINATION_DIR"
+
 # Function to download and verify the checksum
 download_and_verify() {
   echo "Downloading '$BINARY_NAME' from GitHub..."
   curl -o "$BINARY_NAME" -L "$BINARY_URL"
+  
   echo "Downloading checksum..."
   curl -o "checksum.sha256" -L "$CHECKSUM_URL"
-
-  echo "Checksum file content:"
-  cat checksum.sha256
-
-  echo "Binary file checksum:"
-  sha256sum "$BINARY_NAME"
-
+  
   echo "Verifying checksum..."
   if ! sha256sum -c checksum.sha256; then
     echo "❌ Checksum verification failed."
@@ -40,12 +36,16 @@ cp "$BINARY_NAME" "$DESTINATION_PATH"
 echo "Making '$DESTINATION_PATH' executable..."
 chmod +x "$DESTINATION_PATH"
 
+# Confirm completion
+echo "✅ Installation complete! You can now use the '$BINARY_NAME' command."
+
 # Clean up
 rm "$BINARY_NAME" checksum.sha256
+
 # Add ~/bin to the PATH if it's not already in the PATH
-#if echo "$PATH" | grep -q "$HOME/bin"; then
-    #echo "~/bin is already in your PATH."
-#else
+if echo "$PATH" | grep -q "$HOME/bin"; then
+    echo "~/bin is already in your PATH."
+else
     echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
     echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
     if [ -n "$BASH_VERSION" ]; then
@@ -54,5 +54,4 @@ rm "$BINARY_NAME" checksum.sha256
         source ~/.zshrc
     fi
     echo "✅ Installation complete! You can now use the 'cryptonit' command."
-#fi
-
+fi
