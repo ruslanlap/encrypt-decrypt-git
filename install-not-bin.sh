9e4736daf1ea3fbe5fe41c3fccbb8e5d96324f58
ruslanlap <lapin@ucu.edu.ua>
2024-07-19 20:54:59 +0300

    add easy install process

--- install-not-bin.sh
+++ install-not-bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-BINARY_NAME="cryptonit"
+BINARY_NAME="encrypt.sh"
 BINARY_URL="https://raw.githubusercontent.com/ruslanlap/encrypt-decrypt-git/master/encrypt.sh"
 CHECKSUM_URL="https://raw.githubusercontent.com/ruslanlap/encrypt-decrypt-git/master/encrypt.sh.sha256"
 DESTINATION_DIR="$HOME/bin"
@@ -9,12 +9,10 @@ DESTINATION_PATH="$DESTINATION_DIR/$BINARY_NAME"
 
 # Create destination directory if it doesn't exist
 mkdir -p "$DESTINATION_DIR"
-
 # Function to download and verify the checksum
 download_and_verify() {
   echo "Downloading '$BINARY_NAME' from GitHub..."
   curl -o "$BINARY_NAME" -L "$BINARY_URL"
-  
   echo "Downloading checksum..."
   curl -o "checksum.sha256" -L "$CHECKSUM_URL"
 
@@ -25,7 +23,6 @@ download_and_verify() {
   sha256sum "$BINARY_NAME"
 
   echo "Verifying checksum..."
-  # Ensure the checksum file format is correct
   if ! sha256sum -c checksum.sha256; then
     echo "❌ Checksum verification failed."
     exit 1
@@ -48,11 +45,10 @@ echo "✅ Installation complete! You can now use the '$BINARY_NAME' command."
 
 # Clean up
 rm "$BINARY_NAME" checksum.sha256
-
 # Add ~/bin to the PATH if it's not already in the PATH
-if echo "$PATH" | grep -q "$HOME/bin"; then
-    echo "~/bin is already in your PATH."
-else
+#if echo "$PATH" | grep -q "$HOME/bin"; then
+    #echo "~/bin is already in your PATH."
+#else
     echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
     echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
     if [ -n "$BASH_VERSION" ]; then
@@ -61,5 +57,5 @@ else
         source ~/.zshrc
     fi
     echo "✅ Installation complete! You can now use the 'cryptonit' command."
-fi
+#fi
 
